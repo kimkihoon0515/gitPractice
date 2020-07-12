84a707a2225a6dc5fe4ea49183f7ffda84c73d4c
kimkihoon <rlarlgns0515@ajou.ac.kr>
2020-07-13 01:14:54 +0900

    practice 3 code has changed

--- practice3.java
+++ practice3.java
@@ -1,18 +1,24 @@
 package New.Git.Practice;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class practice3 {
-    public static void main(String[]args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int sum = 0;
-        for (int i = 0; i < a; i++) {
-            int b = in.nextInt();
-            int c = in.nextInt();
-            sum = c + b;
-            System.out.println(sum);
+    public static void main(String[]args)throws Exception
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            StringTokenizer st;
+            int TC = Integer.parseInt(br.readLine());
+
+            for(int i=0; i<TC; i++) {
+                st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                int sum = A+B;
+
+                bw.write(sum + "\n");
+            }
+            bw.flush();
         }
-        in.close();
     }
-}
