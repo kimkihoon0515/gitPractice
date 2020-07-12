410a4a63f45c497968afa0cead1033d5bb368a57
kimkihoon <rlarlgns0515@ajou.ac.kr>
2020-07-13 01:20:13 +0900

    quiz num2741 code has uploaded

---
+++ num2741.java
@@ -0,0 +1,16 @@
+package New.Git.Practice;
+
+import java.util.Scanner;
+
+public class num2741 {
+    public static void main(String[]args)
+    {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        in.close();
+        for (int i=1;i<a+1;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
